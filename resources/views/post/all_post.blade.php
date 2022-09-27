de036aac353195b875e9b005c30d8a8b4ce9c36a
aditiyadwiramadani88 <aditiyadwiramadani88@gmail.com>
2022-09-27 22:56:41 +0700

    All Post With Access All

--- resources/views/post/all_post.blade.php
+++ resources/views/post/all_post.blade.php
@@ -2,14 +2,27 @@
 @section('content')
 
 
-<div class="grid place-items-center grid-cols-1 gap-10 xl:grid-cols-3 lg:grid-cols-2 mt-11 2xl:grid-cols-4"> 
+
+<form action="/search">
+<div class="mt-6">
+
+    <div class="flex">
+        
+        <input type="text" name="keyword" id="keyword" value="{{old('keyword')}}"   class="rounded-lg mx-auto bg-white md:w-full w-3/4 h-10 shadow-lg otline-none focus:outline-none focus:ring focus:border-blue-300 ">
+        <button class=" bg-blue-800 text-white w-24 h-10 rounded-lg shadow-lg ml-2" type="submit">Search</button>
+    </div>
+</div>
+</form>
+<div class="grid place-items-center grid-cols-1 gap-10 xl:grid-cols-2 lg:grid-cols-2 mt-11 2xl:grid-cols-3"> 
+    
     @foreach($row as $data)
     <div class=" place-items-center bg-white shadow-md rounded-xl overflow-hidden 2xl:max-w-screen-2xl  max-w-md mx-auto md:max-w-2xl h-48 w-full justify-self-center" >
+        
         <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold ml-2 mt-7 mb-2">
             <a href="/post/{{$data->slug}}" class="hover:underline">{{$data->title}}</a>
         </div>
         <div class="ml-4 mb-5 mr-2">
-            <p class="hidden all_content_get">{{$data->content}}</p>
+            <p class="hidden all_content_get">{{ $data->content }}</p>
             <p class="all_content"></p>
             @if(\App\Models\View::where('slug_post', $data->slug)->first()) 
             <div class="flex justify-self-start inline-block">
@@ -36,9 +49,22 @@
         </div>
 
     </div>
+
     @endforeach
-</div>
+    <script>
+
+        document.querySelector('.leading-5').classList("none")
+    const search_keyword = document.location.search
+    const str_search_replace = search_keyword.replace('?keyword=', '')
+    if(search_keyword = '') { }
+        document.querySelector('.keyword').value = search_keyword.replace('')
+    </script>
+@endsection
 
 
+@section('home')
+<div class="mt-16">   
+        {{$row->links()}}
+    </div>
 @endsection
 
