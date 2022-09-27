51a77e3e56b0ee57ecc90c0cc7f25b155c2240be
aditiyadwiramadani88 <aditiyadwiramadani88@gmail.com>
2022-09-27 22:54:33 +0700

    Comment Controller

--- app/Http/Controllers/Comment.php
+++ app/Http/Controllers/Comment.php
@@ -5,18 +5,19 @@ namespace App\Http\Controllers;
 use App\Models\CommentModel;
 use App\Models\PostModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class Comment extends Controller{
 
     public function CreateComment(Request $request) { 
         $request->validate([ 
-            'name'      => ['required', 'min:4', 'max:20'], 
-            'email'     => ['required', 'email'], 
             'comment'   => ['required']
         ]);
 
-        $get_form = $request->only('name', 'email', 'post_slug', 'comment');
-        
+        $get_form = $request->only('post_slug', 'comment');
+        $get_form['name'] = Auth::user()->name;
+        $get_form['email'] = Auth::user()->email;
+
         if(!PostModel::where('slug',$get_form['post_slug'])->first()) { 
             $request->session()->flash('error', 'errors data not fout');
             return redirect()->back();
