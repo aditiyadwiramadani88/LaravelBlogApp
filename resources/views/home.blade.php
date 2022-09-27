59f14603e2fd4475b9bf90706e0abd006c5f2221
aditiyadwiramadani88 <aditiyadwiramadani88@gmail.com>
2022-09-27 22:54:48 +0700

    Home View

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
 @section('home')
  <div class=" container-lg mx-auto">
       <div class="w-full">
-          <div class="grid md:grid-cols-2  gap-x-86">
+          <div class="grid md:grid-cols-2  gap-x-86 md:ml-4">
               <div class="ml-2 mt-6 pt-16 mx-auto">
                 <p class="text-3xl text-blue-400"><b class="text-blue-400">Blog</b><b class=" text-red-400">Site</b></p>
                 <p class="text-gray-600">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio debitis totam doloribus eligendi minima neque, veniam perferendis voluptates alias suscipit, deleniti cumque, itaque maiores eos? Nam molestias blanditiis debitis veniam. lore Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio debitis totam doloribus eligendi minima neque</p>
